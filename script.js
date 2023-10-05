2f1fca999ef379bc52167424f6150e9913f0d6b9
Ummid2001 <139231974+Ummid2001@users.noreply.github.com>
2023-10-05 22:40:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Second Highest Element</title>
+</head>
+<body>
+    <script>
+        function secondHighest(arr) {
+            if (arr.length < 2) {
+                return -Infinity;
+            }
+
+            let max = -Infinity;
+            let secondMax = -Infinity;
+
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i] > max) {
+                    secondMax = max;
+                    max = arr[i];
+                } else if (arr[i] < max && arr[i] > secondMax) {
+                    secondMax = arr[i];
+                }
+            }
+
+            if (secondMax === -Infinity) {
+                return -Infinity;
+            }
+
+            return secondMax;
+        }
+
+   
+    </script>
+</body>
+</html>
